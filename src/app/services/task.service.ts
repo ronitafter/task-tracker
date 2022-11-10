01a279eed1ae4b825f6e59bc697a250fa26afb18
RONIT AFTER <ronitafter@gmail.com>
2022-11-10 13:09:35 +0200

    new

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from '../mock-tasks';
+// import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+// import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // const tasks = of(TASKS);
+    // return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
