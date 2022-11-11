e9e5163fe43a655a1700511ff7c28918514d8a4f
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-11-11 20:09:05 +0200

    ggg

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -9,13 +9,19 @@ import { TaskService } from 'src/app/services/task.service';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  // tasks: Task[] = TASKS;
   tasks: Task[] = [];
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    // this.tasks = this.taskService.getTasks();
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-// import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 import { Observable } from 'rxjs';
-// import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { TASKS } from '../mock-tasks';
+// import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +14,11 @@ export class TaskService {
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    // const tasks = of(TASKS);
-    // return tasks;
     return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
