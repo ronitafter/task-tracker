a0271d0ecb87ded410c3e6bffd4af21e9a598bd2
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-11-13 20:24:47 +0200

    new

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'task-tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {}
 
   ngOnInit(): void {}
 
   AddTask() {
-    console.log('test');
+    this.uiService.toggleAddTask();
   }
 }
